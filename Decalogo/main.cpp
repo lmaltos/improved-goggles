5a3e67fe0dac9b4c93cf003370cbed5316bda803
lmaltos <136147644+lmaltos@users.noreply.github.com>
2025-03-25 00:27:55 +0000

    Arreglos y pointers

--- Decalogo/main.cpp
+++ Decalogo/main.cpp
@@ -18,9 +18,10 @@ void regla07();
 void regla08();
 void regla09();
 void regla10();
+void arreglos();
 
 int main() {
-    regla10();
+    arreglos();
     return 0;
 }
 
@@ -237,4 +238,32 @@ void regla10() {
     cout << "(*p)--; " << *p << endl;
     *p %= 5;
     cout << "*p %= 5; " << *p << endl;
+}
+
+void arreglos() {
+    int *arreglo;
+    int *p = new int;
+    arreglo = new int[50];
+    cout << "arreglo " << arreglo << endl;
+    for (int i = 0; i < 50; i++) {
+        arreglo[i] = (i+1) * 3;
+    }
+
+    for (int i = 0; i < 50; i++) {
+        cout << "arreglo[" << i << "]: " << arreglo[i] << " &arreglo[i]: " << &arreglo[i] << " ";
+        if (((i+1) % 3) == 0) {
+            cout << endl;
+        }
+    }
+
+    cout << endl;
+    for (int i = 0; i < 50; i++) {
+        cout << "* (arreglo +" << i << "): " << *(arreglo+i) << " arreglo + i: " << arreglo + i << " ";
+        if (((i+1) % 3) == 0) {
+            cout << endl;
+        }
+    }
+    p[0] = 5;
+    delete p;
+    delete[] arreglo;
 }
